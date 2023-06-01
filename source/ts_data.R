b7a103a4e5337eeabbaa578200835e16ce2582b7
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-01 13:45:12 -0300

    Create ts_data.R

---
+++ source/ts_data.R
@@ -0,0 +1,33 @@
+ts_data = function(mu, phi, sigma,
+                   b0, b1, b2,
+                   y0,
+                   v, #( log(v) )
+                   T,
+                   seed = 634323){
+  ############################################################################
+  ################################# SVM-SMN Model ############################
+  # y_t = b0 + b1 * y_{t-1} + b2 * e(h_t) + e_t/lambda_t^{1/2}
+  # h_t = mu + phi * ( h_{t-1} - mu ) + sigma * n_{t}
+  ############################################################################
+  ###################### t-Student: lambda|v ~ Gama(v/2, v/2) 
+  
+  
+  y = h = l = rep(0, T)
+  
+  #data_seed = sample( 1:1e6, 1 )
+  # 634323
+  set.seed( seed )
+  for(t in 1:T){
+    if(t == 1){
+      l[t] = rgamma(1, shape = v/2, rate = v/2)
+      h[t] = rnorm(1, mean = mu, sd = sigma * 1 / sqrt( (1 - phi * phi) ) )
+      y[t] = rnorm(1, b0 + b1 * y0 + b2 * exp( h[t] ), exp(h[t]/2) / sqrt( l[t] ))
+    }else{
+      l[t] = rgamma(1, shape = v/2, rate = v/2)
+      h[t] = rnorm(1, mean = (mu + phi * ( h[t-1] - mu )), sd = sigma)
+      y[t] = rnorm(1, b0 + b1 * y[t-1] + b2 * exp(h[t]), exp(h[t]/2) / sqrt( l[t] ))
+    }
+  }
+  
+  return( y )
+}
