394c1b0e50de97375d396195d09b7a021b5da503
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-03 17:12:21 -0300

    Update estudo_priori.R

--- simulação/estudo1/ts/estudo_priori.R
+++ simulação/estudo1/ts/estudo_priori.R
@@ -1,17 +1,17 @@
 library(rstan)
 options( mc.cores = 4 )
-source('C:/Users/8936381/Documents/source/estudo_priori_stan.R')
+source('https://raw.githubusercontent.com/holtz27/rbras/main/source/estudo_priori_stan.R')
 # n° replicações
-r = 50
+r = 1
 seeds = sample(1:1e6, r)
 # priori 1
-path = 'C:/Users/8936381/Documents/rstan/Estudo_prioris/ts/ts_priori1.stan'
-model1 = stan_model( path )
-priori1 = estudo_priori_stan(y0 = 0,
-                             model = model1,
-                             M = 2.5e3,
+path = 'C:/Users/8936381/Documents/rstan/ts/priori2.stan'
+model = stan_model( path )
+priori2 = estudo_priori_stan(y0 = 0,
+                             model = model,
+                             M = 7.5e3,
                              nchains = 4,
-                             lags = 1,
+                             lags = 5,
                              r = r,
                              seeds = seeds )
 priori1$summary
