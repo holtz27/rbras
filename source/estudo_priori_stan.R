f87f5c27b77d571f246aec44b84f8968218270d3
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-08 15:17:11 -0300

    Update estudo_priori_stan.R

--- source/estudo_priori_stan.R
+++ source/estudo_priori_stan.R
@@ -6,9 +6,7 @@ estudo_priori_stan = function(y0,
                               r, 
                               seeds){
   
-  #source('C:/Users/8936381/Documents/source/ts_data.R')
   source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/ts_data.R' )
-  #source('C:/Users/8936381/Documents/source/svmsmn_stan_fit.R')
   source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/svmsmn_stan_fit.R' )
   
   param = matrix(c(-1.0, 0.985, 0.13,
