a476737a28ca90042e3fbb8c958208c951989151
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-08 19:03:57 -0300

    Update svmsmn_stan_fit.R

--- source/svmsmn_stan_fit.R
+++ source/svmsmn_stan_fit.R
@@ -1,12 +1,18 @@
-svmsmn_stan_fit = function(data, y0, 
-                           model, M, nchains = 3, 
-                           lags = 1){
+svmsmn_stan_fit = function(data, 
+                           y0, 
+                           model, 
+                           warmup
+                           M, 
+                           nchains, 
+                           lags){
   
   fit = sampling(model, 
                  list(T = length( data ), 
                       y = data,
                       y0 = y0),
                  iter = M,
+                 warmup = warmup,
+                 thin = lags,
                  chains = nchains)
   
   parameters = extract( fit )
@@ -18,8 +24,8 @@ svmsmn_stan_fit = function(data, y0,
                     parameters$b2,
                     parameters$v ), nrow = 7, byrow = TRUE)
   # jumps
-  jumps = seq(1, 0.5 * M * nchains, by = lags)
-  draws = draws[, jumps ]
+  #jumps = seq(1, 0.5 * M * nchains, by = lags)
+  #draws = draws[, jumps ]
   
   return( matrix( apply(draws, 1, mean), ncol = 1 ) )
 }
