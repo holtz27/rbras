27f2ac0aef9413bb2e1eb63a0820ebcbb1ec42d7
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-08 16:55:11 -0300

    Update normal_data.R

--- source/normal_data.R
+++ source/normal_data.R
@@ -1,39 +1,27 @@
 function(mu, phi, sigma,
         b0, b1, b2,
-        y0, T, seed){
-############################################################################
-################################# SVM-SMN Model ############################
-# y_t = b0 + b1*y_{t-1} + b2*e(h_t) + e_t/lambda_t^{1/2}
-# h_t = mu + phi(h_{t-1} - mu) + sigma*n_{t}
-############################################################################
-###################### Normal: l ~ 1 
+        y0, 
+        T,
+        seed){
+ ############################################################################
+ ################################# SVM-SMN Model ############################
+ # y_t = b0 + b1*y_{t-1} + b2*e(h_t) + e_t/lambda_t^{1/2}
+ # h_t = mu + phi(h_{t-1} - mu) + sigma*n_{t}
+ ############################################################################
+ ###################### Normal: l ~ 1 
+ y = h = rep(0, T)
+ set.seed( seed )
 
-mu = -1
-phi = 0.985
-sigma = 0.13
-b0 = 0.08
-b1 = 0.15
-b2 = - 0.023
-y0 = 0
-T = 2e3
-y = h = rep(0, T)
+ for(t in 1:T){
+   if(t == 1){
+     h[t] = rnorm(1, mean = mu, sd = sigma * sqrt( 1 / ( 1 - phi**2 ) ))
+     y[t] = rnorm(1, b0 + b1 * y0 + b2 * exp(h[t]), exp(h[t]/2) )
+   }else{
+     h[t] = rnorm(1, mean = (mu + phi * ( h[t-1] - mu) ), sd = sigma)
+     y[t] = rnorm(1, b0 + b1*y[t - 1] + b2 * exp(h[t]), exp(h[t]/2) )
+   }
+ }
 
-#seed = sample(1:1e6, 1)
-set.seed( 788284 )
-
-for(t in 1:T){
-  if(t == 1){
-    h[t] = rnorm(1, mean = mu, sd = sigma*sqrt(1/(1-phi**2)))
-    y[t] = rnorm(1, b0 + b1 * y0 + b2 * exp(h[t]), exp(h[t]/2) )
-  }else{
-    h[t] = rnorm(1, mean = (mu + phi * ( h[t-1] - mu) ), sd = sigma)
-    y[t] = rnorm(1, b0 + b1*y[t - 1] + b2 * exp(h[t]), exp(h[t]/2) )
-  }
 }
-
-par(mfrow = c(1, 2))
-plot(y, type = 'l', main = 'log - retornos')
-hist(y, breaks = 40, main = 'histograma')
-par(mfrow = c(1, 1))
 
 
