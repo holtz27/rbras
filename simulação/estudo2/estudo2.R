6072881be4c091c6d48acfaa83436d1693593ba5
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-13 14:56:33 -0300

    Update estudo2.R

--- simulação/estudo2/estudo2.R
+++ simulação/estudo2/estudo2.R
@@ -2,17 +2,248 @@ library(rstan)
 options( mc.cores = 4 )
 ################################################################################
 # stan models
-path0 = 'C:/Users/8936381/Documents/rstan/normal/stan_normal_model.stan'
-normal_fit = stan_model( path0 )
+normal_fit = stan_model( model_code = 'data{
+                                              int<lower=0> T;
+                                              real y0;
+                                              vector[T] y;
+                                            }
+                                            parameters{
+                                              real mu;                         // mean log volatility
+                                              real<lower=-1,upper=1> phiT;     // persistence of volatility
+                                              real<lower=0> s2;
+                                              vector[T] h_std;                 // std log volatility time t
+                                              real<lower=-1,upper=1> b1T;
+                                              real b0;
+                                              real b2;
+                                            }
+                                            transformed parameters{
+                                              real<lower=-1,upper=1> phi;
+                                              real<lower=-1,upper=1> b1;
+                                              real<lower=0> sigma;
+                                              vector[T] h;  // now h ~ normal(0, sigma)
+                                              vector[T] mu_t;
+                                              phi = (2*phiT - 1);
+                                              b1 = (2*b1T - 1);
+                                              sigma = sqrt(s2);
+                                              
+                                              //--- Volatilitys:
+                                              h = h_std * sigma;
+                                              h[1] /= sqrt(1 - phi * phi);  // rescale h[1]
+                                              h += mu;
+                                              for (t in 2:T) {
+                                                h[t] += phi * (h[t - 1] - mu);
+                                              }
+                                              
+                                              //--- Means:
+                                              mu_t[1] = b0 + y0*b1 + exp( h[1] ) * b2;
+                                              for(t in 2:T){
+                                                mu_t[t] = b0 + b1 * y[t-1]  + exp( h[t] ) * b2;
+                                              }
+                                            }
+                                            model{
+                                              // --- prioris
+                                              mu ~ normal(0, 3.2);
+                                              phiT ~ beta(20, 1.5);
+                                              b0 ~ normal(0, 3.2);
+                                              b1T ~ beta(5, 1.5);
+                                              b2 ~ normal(0, 3.2);
+                                              s2 ~ inv_gamma(2.5, 0.025);
+                                              
+                                              //--- Sampling volatilitys:
+                                              h_std ~ std_normal();
+                                              
+                                              //--- Sampling observations:
+                                              y ~ normal( mu_t, exp(h/2) );
+                                            }
+                                            ' )
+
 # ts
-path1 = 'C:/Users/8936381/Documents/rstan/ts/stan_ts_model.stan'
-ts_fit = stan_model( path1 )
+ts_fit = stan_model( model_code = 'data{
+                                        int<lower=0> T;
+                                        real y0;
+                                        vector[T] y;
+                                      }
+                                      parameters{
+                                        real mu;                         // mean log volatility
+                                        real<lower=-1,upper=1> phiT;     // persistence of volatility
+                                        real<lower=0> s2;
+                                        vector[T] h_std;  // std log volatility time t
+                                        vector<lower=0>[T] l;
+                                        real<lower=0.02,upper=0.5> lambda;
+                                        real<lower=0> v; 
+                                        real<lower=-1,upper=1> b1T;
+                                        real b0;
+                                        real b2;
+                                      }
+                                      transformed parameters{
+                                        real<lower=-1,upper=1> phi;
+                                        real<lower=-1,upper=1> b1;
+                                        real<lower=0> sigma;
+                                        vector[T] h;  // now h ~ normal(0, sigma)
+                                        vector[T] mu_t;
+                                        phi = (2*phiT - 1);
+                                        b1 = (2*b1T - 1);
+                                        sigma = sqrt(s2);
+                                        
+                                        //--- Volatilitys:
+                                        h = h_std * sigma;
+                                        h[1] /= sqrt(1 - phi * phi);  // rescale h[1]
+                                        h += mu;
+                                        for (t in 2:T) {
+                                          h[t] += phi * (h[t - 1] - mu);
+                                        }
+                                        
+                                        //--- Means:
+                                        mu_t[1] = b0 + y0 * b1 + exp( h[1] ) * b2;
+                                        for(t in 2:T){
+                                          mu_t[t] = b0 + b1 * y[t-1]  + exp( h[t] ) * b2;
+                                        }
+                                      }
+                                      model{
+                                        // --- prioris
+                                        mu ~ normal(0, 3.2);
+                                        phiT ~ beta(20, 1.5);
+                                        b0 ~ normal(0, 3.2);
+                                        b1T ~ beta(5, 1.5);
+                                        b2 ~ normal(0, 3.2);
+                                        s2 ~ inv_gamma(2.5, 0.025);
+                                        // priori 1 
+                                        //v ~ gamma(12, 0.8);
+                                        // priori 2
+                                        //target += 0.5 * log( v /( v+3 ) ) + 0.5 * log( trigamma( 0.5 * v ) - trigamma( 0.5 * (v+1) ) - 2 * ( v + 3 ) / ( v * square(v+1) ) );
+                                        // priori 3
+                                        lambda ~ uniform(0.02, 0.5);
+                                        v ~ exponential( lambda );
+                                        
+                                        //--- Sampling volatilitys:
+                                        h_std ~ std_normal();
+                                        for(t in 1:T) l[t] ~ gamma(0.5 * v, 0.5 * v);
+                                        
+                                        //--- Sampling observations:
+                                        //y ~ normal( mu_t, exp(h/2) * l );
+                                        for(t in 1:T) y[t] ~ normal( mu_t[t], exp( 0.5 * h[t] ) / sqrt( l[t] ) );
+                                      }
+                                      ' )
 # slash
-path2 = 'C:/Users/8936381/Documents/rstan/slash/stan_slash_model.stan'
-slash_fit = stan_model( path2 )
+slash_fit = stan_model( model_code = 'data{
+                                          int<lower=0> T;
+                                          real y0;
+                                          vector[T] y;
+                                        }
+                                        parameters{
+                                          real mu;                         // mean log volatility
+                                          real<lower=-1,upper=1> phiT;     // persistence of volatility
+                                          real<lower=0> s2;
+                                          vector[T] h_std;  // std log volatility time t
+                                          vector<lower=0, upper=1> [T] l;
+                                          real<lower=0> v; 
+                                          real<lower=-1,upper=1> b1T;
+                                          real b0;
+                                          real b2;
+                                        }
+                                        transformed parameters{
+                                          real<lower=-1,upper=1> phi;
+                                          real<lower=-1,upper=1> b1;
+                                          real<lower=0> sigma;
+                                          vector[T] h;  // now h ~ normal(0, sigma)
+                                          vector[T] mu_t;
+                                          phi = (2*phiT - 1);
+                                          b1 = (2*b1T - 1);
+                                          sigma = sqrt(s2);
+                                          
+                                          //--- Volatilitys:
+                                          h = h_std * sigma;
+                                          h[1] /= sqrt(1 - phi * phi);  // rescale h[1]
+                                          h += mu;
+                                          for (t in 2:T) {
+                                            h[t] += phi * (h[t - 1] - mu);
+                                          }
+                                          
+                                          //--- Means:
+                                          mu_t[1] = b0 + y0 * b1 + exp( h[1] ) * b2;
+                                          for(t in 2:T){
+                                            mu_t[t] = b0 + b1 * y[t-1]  + exp( h[t] ) * b2;
+                                          }
+                                        }
+                                        model{
+                                          // --- prioris
+                                          mu ~ normal(0, 3.2);
+                                          phiT ~ beta(20, 1.5);
+                                          b0 ~ normal(0, 3.2);
+                                          b1T ~ beta(5, 1.5);
+                                          b2 ~ normal(0, 3.2);
+                                          s2 ~ inv_gamma(2.5, 0.025);
+                                          v ~ gamma(2.0, 0.25);
+                                          
+                                          //--- Sampling volatilitys:
+                                          h_std ~ std_normal();
+                                          for(t in 1:T) l[t] ~ beta( v, 1 );
+                                          
+                                          //--- Sampling observations:
+                                          //y ~ normal( mu_t, exp(h/2) * l );
+                                          for(t in 1:T) y[t] ~ normal( mu_t[t], exp( 0.5 * h[t] ) / sqrt( l[t] ) );
+                                        }
+                                        ' )
 # vgamma
-path3 = 'C:/Users/8936381/Documents/rstan/vgama/stan_vgama_model.stan'
-vgama_fit = stan_model( path3 )
+vgama_fit = stan_model( model_code = 'data{
+                                          int<lower=0> T;
+                                          real y0;
+                                          vector[T] y;
+                                        }
+                                        parameters{
+                                          real mu;                         // mean log volatility
+                                          real<lower=-1,upper=1> phiT;     // persistence of volatility
+                                          real<lower=0> s2;
+                                          vector[T] h_std;  // std log volatility time t
+                                          vector<lower=0> [T] l;
+                                          real<lower=0> v; 
+                                          real<lower=-1,upper=1> b1T;
+                                          real b0;
+                                          real b2;
+                                        }
+                                        transformed parameters{
+                                          real<lower=-1,upper=1> phi;
+                                          real<lower=-1,upper=1> b1;
+                                          real<lower=0> sigma;
+                                          vector[T] h;  // now h ~ normal(0, sigma)
+                                          vector[T] mu_t;
+                                          phi = (2*phiT - 1);
+                                          b1 = (2*b1T - 1);
+                                          sigma = sqrt(s2);
+                                          
+                                          //--- Volatilitys:
+                                          h = h_std * sigma;
+                                          h[1] /= sqrt(1 - phi * phi);  // rescale h[1]
+                                          h += mu;
+                                          for (t in 2:T) {
+                                            h[t] += phi * (h[t - 1] - mu);
+                                          }
+                                          
+                                          //--- Means:
+                                          mu_t[1] = b0 + y0 * b1 + exp( h[1] ) * b2;
+                                          for(t in 2:T){
+                                            mu_t[t] = b0 + b1 * y[t-1]  + exp( h[t] ) * b2;
+                                          }
+                                        }
+                                        model{
+                                          // --- prioris
+                                          mu ~ normal(0, 3.2);
+                                          phiT ~ beta(20, 1.5);
+                                          b0 ~ normal(0, 3.2);
+                                          b1T ~ beta(5, 1.5);
+                                          b2 ~ normal(0, 3.2);
+                                          s2 ~ inv_gamma(2.5, 0.025);
+                                          v ~ gamma(2.0, 0.25);
+                                          
+                                          //--- Sampling volatilitys:
+                                          h_std ~ std_normal();
+                                          for(t in 1:T) l[t] ~ inv_gamma( 0.5 * v, 0.5 * v );
+                                          
+                                          //--- Sampling observations:
+                                          //y ~ normal( mu_t, exp(h/2) * l );
+                                          for(t in 1:T) y[t] ~ normal( mu_t[t], exp( 0.5 * h[t] ) / sqrt( l[t] ) );
+                                        }
+                                        ' )
 ################################################################################
 # Running stan code
 source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/svmsmn_stan_fitII.R' )
