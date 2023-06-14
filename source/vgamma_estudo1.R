493e5d7d2f1abac621304a7aaef86fc8536440d9
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-14 17:46:08 -0300

    Update vgamma_estudo1.R

--- source/vgamma_estudo1.R
+++ source/vgamma_estudo1.R
@@ -14,13 +14,10 @@ vgamma_estudo1 = function(M,
                    seeds = NULL){
   source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/data/vgamma_data.R' )
   v1 = v2 = v3 = rep(0, r)
-  
   for( v in tails ){
     if( v == tails[1] ) time.init = Sys.time()
     for( i in 1:r ){
-      
       cat( paste0('réplica ', i, ' com o parâmetro v = ', v ) )
-      
       # Data generation
       data.gen = vgamma_data(mu = mu, phi = phi, sigma = sigma,
                              b0 = b0, b1 = b1, b2 = b2,
@@ -91,11 +88,11 @@ vgamma_estudo1 = function(M,
     vies = matrix( c(vies1 = mean( v1 - v ),
                      vies2 = mean( v2 - v ),
                      vies3 = mean( v3 - v )), ncol = 1 )
-    vies = round( vies, digits = 3 )
+    #vies = round( vies, digits = 3 )
     smse = matrix( c(smse1 = mean( (v1 - v)**2 ),
                      smse2 = mean( (v2 - v)**2 ),
                      smse3 = mean( (v3 - v)**2 )), ncol = 1)
-    smse = round( smse, digits = 3 )
+    #smse = round( smse, digits = 3 )
     
     if( v == tails[1] ){
       summary = data.frame(vies, smse)
