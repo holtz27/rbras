692f020c87204f31b8183a7d7decf1263896195d
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-14 17:29:49 -0300

    Update slash_estudo1.R

--- source/slash_estudo1.R
+++ source/slash_estudo1.R
@@ -1,17 +1,18 @@
 slash_estudo1 = function(M,
-                   warmup,
-                   model1, model2, model3,
-                   r,
-                   tails,
-                   mu,
-                   phi,
-                   sigma,
-                   b0,
-                   b1,
-                   b2,
-                   y0,
-                   T){
-  
+                         warmup,
+                         model1, model2, model3,
+                         r,
+                         tails,
+                         mu,
+                         phi,
+                         sigma,
+                         b0,
+                         b1,
+                         b2,
+                         y0,
+                         T,
+                         seeds = NULL){
+  source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/data/slash_data.R' )
   v1 = v2 = v3 = rep(0, r)
   
   for( v in tails ){
@@ -19,23 +20,14 @@ slash_estudo1 = function(M,
     for( i in 1:r ){
       
       cat( paste0('réplica ', i, ' com o parâmetro v = ', v ) )
-      
       # Data generation
-      y = h = l = rep(0, T)
-      data_seed = sample( 1:1e6, 1 )
-      set.seed( data_seed )
-      for(t in 1:T){
-        if(t == 1){
-          l[t] = rbeta(1, v, 1)
-          h[t] = rnorm(1, mean = mu, sd = sigma * 1 / sqrt( (1 - phi * phi) ) )
-          y[t] = rnorm(1, b0 + b1 * y0 + b2 * exp( h[t] ), exp(h[t]/2) / sqrt( l[t] ))
-        }else{
-          l[t] = rbeta(1, v, 1)
-          h[t] = rnorm(1, mean = (mu + phi * ( h[t-1] - mu )), sd = sigma)
-          y[t] = rnorm(1, b0 + b1 * y[t-1] + b2 * exp(h[t]), exp(h[t]/2) / sqrt( l[t] ))
-        }
-      }
-      
+      data.gen = slash_data(mu = mu, phi = phi, sigma = sigma,
+                            b0 = b0, b1 = b1, b2 = b2,
+                            y0 = y0, v = v, T = T, seed = seeds[ i ])
+      # Data generation
+      y = data.gen$y
+      h = data.gen$h
+      l = data.gen$l
       # models fit
       fit1 = sampling(model1, 
                       list(T = T, 
@@ -94,16 +86,14 @@ slash_estudo1 = function(M,
       cat( '\r' )
       
     }
-    
     vies = matrix( c(vies1 = mean( v1 - v ),
                      vies2 = mean( v2 - v ),
                      vies3 = mean( v3 - v )), ncol = 1 )
     vies = round( vies, digits = 3 )
-    smse = matrix( c(smse1 = mean( (v1 - v)**2 ),
+    #smse = matrix( c(smse1 = mean( (v1 - v)**2 ),
                      smse2 = mean( (v2 - v)**2 ),
                      smse3 = mean( (v3 - v)**2 )), ncol = 1)
-    smse = round( smse, digits = 3 )
-    
+    #smse = round( smse, digits = 3 )
     if( v == tails[1] ){
       summary = data.frame(vies, smse)
       summary = rbind(c('v', v), summary)
@@ -112,10 +102,7 @@ slash_estudo1 = function(M,
       data = rbind(c('v', v), data)
       summary = cbind(summary, data)
     }
-    
-    
     if( v == tails[ length(tails) ] ) time.final = Sys.time()
-    
   } 
   
   row.names( summary ) = c('','priori1', 'priori2','priori3' )
