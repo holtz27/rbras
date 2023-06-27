1ddf596feb8df99bdb33eab97ed289329a7dfcb9
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-27 16:42:07 -0300

    Update svmsmn_stan_fitII.R

--- source/svmsmn_stan_fitII.R
+++ source/svmsmn_stan_fitII.R
@@ -1,8 +1,9 @@
 svmsmn_stan_fitII = function(data, 
                              y0, 
                              model, 
-                             M, 
-                             nchains = 3, 
+                             M,
+                             warmup,
+                             nchains = 1, 
                              lags = 1,
                              model_name,
                              normal,
@@ -14,11 +15,18 @@ svmsmn_stan_fitII = function(data,
                  list(T = length( data ), 
                       y = data,
                       y0 = y0),
-                 iter = M,
+                 iter = warmup + M,
+                 warmup = warmup,
+                 thin = lags,
                  chains = nchains)
   parameters = extract( fit )
   source( 'https://raw.githubusercontent.com/holtz27/rbras/main/source/model_selection.R' )
-  
+  ############################################################################
+  ############################## Model Selection
+  # p( y | theta ) = p( y | b, theta_h, v, h ) = p( y | b, h )
+  # data = ( y0 , y )
+  # param_hat = ( b = b_hat, h = h_hat, l = l_hat )
+  ############################################################################
   if( normal ){
     # Theta draws
     draws = matrix(c( parameters$mu,
@@ -28,10 +36,7 @@ svmsmn_stan_fitII = function(data,
                       parameters$b1, 
                       parameters$b2 ), nrow = 6, byrow = TRUE)
     draws = rbind( draws, t( as.matrix( parameters$h ) ) )
-    draws = rbind( draws, matrix(1, nrow = T, ncol = length(parameters$mu)) )
-    # jumps
-    jumps = seq(1, 0.5 * M * nchains, by = lags)
-    draws = draws[, jumps ]
+    draws = rbind( draws, matrix(1, nrow = T, ncol = length(parameters$mu)) )  
     # model selection
     dic = svmsmn_dic(data = y, y0 = 0, 
                      param_draws = draws[4:(nrow(draws)), ])
@@ -53,15 +58,6 @@ svmsmn_stan_fitII = function(data,
                       parameters$v ), nrow = 7, byrow = TRUE)
     draws = rbind( draws, t( as.matrix( parameters$h ) ) )
     draws = rbind( draws, t( as.matrix( parameters$l ) ) )
-    # jumps
-    jumps = seq(1, 0.5 * M * nchains, by = lags)
-    draws = draws[, jumps ]
-    ############################################################################
-    ############################## Model Selection
-    # p( y | theta ) = p( y | b, theta_h, v, h ) = p( y | b, h )
-    # data = ( y0 , y )
-    # param_hat = ( b = b_hat, h = h_hat, l = l_hat )
-    ############################################################################
     ############### DIC
     dic = svmsmn_dic(data = y, y0 = 0, 
                      param_draws = draws[ c( 4:6, 
