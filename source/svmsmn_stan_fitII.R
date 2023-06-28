e81327e494b499e6770df2938587c855d4c7a2b1
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-06-28 15:37:02 -0300

    Update svmsmn_stan_fitII.R

--- source/svmsmn_stan_fitII.R
+++ source/svmsmn_stan_fitII.R
@@ -37,6 +37,14 @@ svmsmn_stan_fitII = function(data,
                       parameters$b2 ), nrow = 6, byrow = TRUE)
     draws = rbind( draws, t( as.matrix( parameters$h ) ) )
     draws = rbind( draws, matrix(1, nrow = T, ncol = length(parameters$mu)) )  
+    # convergence check
+    ############### Numeric Analysis
+    ############################### theta
+    mcmc = coda::as.mcmc( t( draws[1:6, ] ) )
+    ####### Geweke Statistic
+    # |G| > 1.96 evidencia não convergencia
+    CD_theta = coda::geweke.diag( mcmc )
+    convergence = sum( as.numeric( c( abs( CD_theta$z ) < 1.96 ) ) ) == 6 
     # model selection
     dic = svmsmn_dic(data = y, y0 = 0, 
                      param_draws = draws[4:(nrow(draws)), ])
@@ -46,7 +54,7 @@ svmsmn_stan_fitII = function(data,
     ############### loo
     loo = svmsmn_loo(data = y, y0 = 0, 
                      draws = draws[4:(nrow(draws)), ],
-                    cores = cores)
+                     cores = cores)
   }else{
     # Theta draws
     draws = matrix(c( parameters$mu,
@@ -58,6 +66,14 @@ svmsmn_stan_fitII = function(data,
                       parameters$v ), nrow = 7, byrow = TRUE)
     draws = rbind( draws, t( as.matrix( parameters$h ) ) )
     draws = rbind( draws, t( as.matrix( parameters$l ) ) )
+    # convergence check
+    ############### Numeric Analysis
+    ############################### theta
+    mcmc = coda::as.mcmc( t( draws[1:7, ] ) )
+    ####### Geweke Statistic
+    # |G| > 1.96 evidencia não convergencia
+    CD_theta = coda::geweke.diag( mcmc )
+    convergence = sum( as.numeric( c( abs( CD_theta$z ) < 1.96 ) ) ) == 7
     ############### DIC
     dic = svmsmn_dic(data = y, y0 = 0, 
                      param_draws = draws[ c( 4:6, 
@@ -82,10 +98,11 @@ svmsmn_stan_fitII = function(data,
   model_selection = rbind( model_selection, 
                            c(dic,
                              waic$estimates[3,1],
-                             loo$estimates[3,1])
+                             loo$estimates[3,1],
+                             convergence)
   )
   row.names( model_selection ) = model_name
-  colnames( model_selection ) = c( 'dic', 'waic', 'loo' )
+  colnames( model_selection ) = c( 'dic', 'waic', 'loo', 'convergence' )
   
   return( model_selection )
 }
